2b70a95184ed4ecd560f6cd591028b705216af9f
redd096 <martin.babino96@gmail.com>
2021-02-25 10:22:30 +0100

    inserite librerie

---
+++ Assets/Scripts/redd096/TileGrid/TileColorMap.cs
@@ -0,0 +1,12 @@
+﻿namespace redd096
+{
+    using UnityEngine;
+
+    [AddComponentMenu("redd096/TileGrid/Tile Color Map")]
+    [SelectionBase]
+    public class TileColorMap : TileBase
+    {
+        [Header("Color Map")]
+        public Color tileColor = Color.white;
+    }
+}
